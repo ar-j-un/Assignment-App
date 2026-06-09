765a32b08ff810f4152705c39013aa1818619b36
ar-j-un <arjun.menon@prophaze.com>
2026-06-09 16:52:30 +0530

    feat(routes): add recipe create and store routes
    - Register GET /recipes/create route mapped to RecipeController@create named recipes.create
    - Register POST /recipes route mapped to RecipeController@store named recipes.store
    - Apply auth middleware explicitly to both routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/profile', [ProfileController::class, 'view'])->name('profile')->middleware('auth');
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update')->middleware('auth');
+
+Route::get('/recipes/create', [RecipeController::class, 'create'])->name('recipes.create')->middleware('auth');
+Route::post('/recipes', [RecipeController::class, 'store'])->name('recipes.store')->middleware('auth');
