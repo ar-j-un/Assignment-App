3d0de07a606ce2c131679c34984c1bb8562ea3e9
ar-j-un <arjun.menon@prophaze.com>
2026-06-16 16:51:38 +0530

    Revert "chore(layout): add styles and vendor-scripts stacks"
    This reverts commit ba64efc2c8d91c8c9cf4929c949e51b47bc00dad.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Welcome') | Laravel App</title>
-    @stack('styles')
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('adminlte/css/adminlte.min.css') }}">
     @vite(['resources/css/app.css'])
@@ -44,7 +44,6 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('adminlte/js/adminlte.min.js') }}"></script>
-    @stack('vendor-scripts')
     @stack('scripts')
 </body>
 </html>
