6349da86f7047cb7e62ff8ab5268e469c4699580
ar-j-un <arjun.menon@prophaze.com>
2026-06-16 15:17:54 +0530

    chore(layout): add jQuery CDN to app layout
    Include jQuery 3.7.1 globally in the main app template to support AJAX functionality across the site.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,7 @@
             </div>
         </main>
     </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('adminlte/js/adminlte.min.js') }}"></script>
     @stack('scripts')
