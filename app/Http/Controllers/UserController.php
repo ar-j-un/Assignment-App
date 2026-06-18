aa23a91d325675d545d7b9ccbc6879add4b475ea
ar-j-un <arjun.menon@prophaze.com>
2026-06-18 11:02:34 +0530

    feat(user): fetch and pass all records to index view
    Update the index method to retrieve the entire users collection from the database using Eloquent.
    
    - Inject the Request instance into the controller method
    - Fetch all users using User::all()
    - Pass the dataset to the users Blade view via compact

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('users');
+        $users = User::all();
+
+        return view('users', compact('users'));
     }
 }
