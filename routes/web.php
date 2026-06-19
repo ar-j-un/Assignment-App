e69d2dccb6e8477e700ee8a9f16b03227e41162b
ar-j-un <arjun.menon@prophaze.com>
2026-06-19 12:01:43 +0530

    feat(route): define PUT route for user update
    Register the /users/{id} route mapping to UserController@update to handle user record modification submissions.

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.up
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::delete('/users/delete/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
 Route::resource('recipes', RecipeController::class)
